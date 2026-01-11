6730694be16fbaa6916c8175d09807df79399330
emiliocayambe <emiliocayambe@gmail.com>
2026-01-10 20:15:22 -0500

    index ready

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+document.getElementById("lastModified").innerHTML = document.lastModified;
+// use the date object  
+const today = new Date();
+document.getElementById("currentyear").innerHTML = today.getFullYear();
